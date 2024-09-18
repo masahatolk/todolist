ddf186ba22067fbf82ca24f012085363c496dc01
Tolkacheva Maria <141151654+masahatolk@users.noreply.github.com>
2024-09-19 02:06:58 +0700

    works

--- app/src/main/java/com/hits/todolist/MainActivity.kt
+++ app/src/main/java/com/hits/todolist/MainActivity.kt
@@ -2,24 +2,18 @@ package com.hits.todolist
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Environment
-import android.provider.DocumentsContract
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ListView
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
 import androidx.navigation.ui.AppBarConfiguration
 import com.hits.todolist.databinding.ActivityMainBinding
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.encodeToJsonElement
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileWriter
-import java.io.Writer
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var listView: ListView
     private val list = ArrayList<Task>()
+    private lateinit var adapter: TodolistAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,8 +35,7 @@ class MainActivity : AppCompatActivity() {
 
         listView = binding.listView
 
-
-        val adapter = TodolistAdapter(this, list)
+        adapter = TodolistAdapter(this, list)
         listView.adapter = adapter
 
         binding.fab.setOnClickListener {
@@ -59,54 +53,56 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.open -> openJson()
-            R.id.save -> saveJson()
+            R.id.save -> save()
         }
         return super.onOptionsItemSelected(item)
     }
 
-    private val launcher = registerForActivityResult<Intent, ActivityResult>(
+    private val launcher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result: ActivityResult ->
         if (result.resultCode == RESULT_OK) {
             val data = result.data
-            Log.d("TREST", "data: $data")
+            val uri = data?.data ?: return@registerForActivityResult
+
+            val file = contentResolver.openInputStream(uri) ?: return@registerForActivityResult
+            val xxx: XXX = Json.decodeFromString(file.bufferedReader().readText())
+            list.clear()
+            list.addAll(xxx.aaa)
+            adapter.notifyDataSetChanged()
         }
     }
 
 
     private fun openJson() {
-        val uri = FileProvider.getUriForFile(this, "${applicationContext.packageName}.provider", getExternalFilesDir("/Android/")!!)
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
             setType("*/*")
-            putExtra(DocumentsContract.EXTRA_INITIAL_URI, uri)
         }
         launcher.launch(intent)
     }
 
-    private fun saveJson() {
-        save(Json.encodeToJsonElement(list).toString())
-    }
+    @Serializable
+    data class XXX(val aaa: ArrayList<Task>)
 
-    private fun save(jsonString: String) {
-        val output: Writer
-        val file = createFile()
-        output = BufferedWriter(FileWriter(file))
-        output.write(jsonString)
-        output.close()
-    }
+    private val launcher2 = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode == RESULT_OK) {
+            val data = result.data
+            val uri = data?.data ?: return@registerForActivityResult
 
-    private fun createFile(): File {
-        val fileName = "todolistData"
-        val storageDir = getExternalFilesDir("/Android/")!!
-
-        if (!storageDir.exists()) {
-            storageDir.mkdir()
+            val writer = contentResolver.openOutputStream(uri)?.bufferedWriter()
+            writer?.write(Json.encodeToString(XXX(list)))
+            writer?.close()
         }
+    }
 
-        return File.createTempFile(
-            fileName,
-            ".json",
-            storageDir
-        )
+    private fun save() {
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/json"
+            putExtra(Intent.EXTRA_TITLE, "data.json")
+        }
+        launcher2.launch(intent)
     }
 }
